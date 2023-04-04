0071d2e5c611f68016e96737d9eb81f9aa7d7881
Alex <103459735+torn80beta@users.noreply.github.com>
2023-04-04 15:33:44 +0300

    profile

--- src/components/social-profile/Profile.jsx
+++ src/components/social-profile/Profile.jsx
@@ -4,16 +4,10 @@ import PropTypes from 'prop-types';
 export const Profile = ({
   userData: { avatar, location, stats, tag, username },
 }) => {
-  //   console.log(username);
   return (
-    // console.log(userData),
     <div className={css.profile}>
       <div className={css.description}>
-        <img
-          src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
-          alt="User avatar"
-          className={css.avatar}
-        />
+        <img src={avatar} alt="User avatar" className={css.avatar} />
         <p className={css.name}>{username}</p>
         <p className={css.tag}>{tag}</p>
         <p className={css.location}>{location}</p>
@@ -22,15 +16,15 @@ export const Profile = ({
       <ul className={css.stats}>
         <li>
           <span className={css.label}>Followers</span>
-          <span className={css.quantity}>1000</span>
+          <span className={css.quantity}>{stats.followers}</span>
         </li>
         <li>
           <span className={css.label}>Views</span>
-          <span className={css.quantity}>2000</span>
+          <span className={css.quantity}>{stats.views}</span>
         </li>
         <li>
           <span className={css.label}>Likes</span>
-          <span className={css.quantity}>3000</span>
+          <span className={css.quantity}>{stats.likes}</span>
         </li>
       </ul>
     </div>
