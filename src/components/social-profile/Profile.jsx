2e65f969a0fab3c6a2f6aeee1b391f91a8cc8064
Alex <103459735+torn80beta@users.noreply.github.com>
2023-04-04 15:35:23 +0300

    tag

--- src/components/social-profile/Profile.jsx
+++ src/components/social-profile/Profile.jsx
@@ -9,7 +9,7 @@ export const Profile = ({
       <div className={css.description}>
         <img src={avatar} alt="User avatar" className={css.avatar} />
         <p className={css.name}>{username}</p>
-        <p className={css.tag}>{tag}</p>
+        <p className={css.tag}>@{tag}</p>
         <p className={css.location}>{location}</p>
       </div>
 
