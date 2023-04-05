a1ee327247418454dd38d71ce6ce392a1dc46463
Alex <103459735+torn80beta@users.noreply.github.com>
2023-04-05 17:25:40 +0300

    stats styles

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,23 +1,24 @@
+import css from '../../components/Statistics/Statistics.module.css';
 import PropTypes, { shape } from 'prop-types';
 
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+const getRandomDarkHexColor = () => {
+  return `hsl(${Math.floor(Math.random() * 360)}deg, 75%, 25%)`;
+};
 
 export const Statistics = ({ title, stats }) => {
   return (
-    <section className="statistics">
-      {title && <h2 className="title">{title}</h2>}
+    <section className={css.statistics}>
+      {title && <h2 className={css.title}>{title}</h2>}
 
-      <ul className="stat-list">
+      <ul className={css.statList}>
         {stats.map(stat => (
           <li
-            className="item"
+            className={css.item}
             key={stat.id}
-            style={{ backgroundColor: getRandomHexColor() }}
+            style={{ backgroundColor: getRandomDarkHexColor() }}
           >
-            <span className="label">{stat.label}</span>
-            <span className="percentage">{stat.percentage}%</span>
+            <span className={css.label}>{stat.label}</span>
+            <span className={css.percentage}>{stat.percentage}%</span>
           </li>
         ))}
       </ul>
